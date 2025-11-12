09eed7acc17cc5492cb967ce687e22188161d94c
JonLee <leeqvip@gmail.com>
2025-11-12 15:48:08 +0800

    init: A Go logging library

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # log
 A Go logging library
+
+## Installation
+
+The only requirement is the [Go Programming Language](https://golang.org/dl/)
+
+```
+go get -u github.com/go-think/log
+```
+
+## License
+
+This project is licensed under the [Apache 2.0 license](LICENSE).
---
+++ handler/rotate_test.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/go-think/log/record"
+)
+
+func TestRotateHandler(t *testing.T) {
+	filename := path.Join(os.TempDir(), "thinkgo.log")
+
+	h := NewRotateHandler(filename, record.DEBUG)
+	filename = h.GetFilename()
+
+	os.Remove(filename)
+
+	message := "Log write to file"
+	r := getRecord(message)
+	h.Handle(r)
+
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Error(err)
+	}
+	content := string(b)
+
+	if !strings.Contains(content, message) {
+		t.Error("test FileHandler error")
+	}
+	t.Log(filename, content)
+}
